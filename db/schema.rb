20c3ddb22aef43a9e4a1d527d3e5a60ddabb0834
Cortland Miller <cortlandm99@gmail.com>
2025-03-03 20:13:20 -0600

    Created Dog model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_04_021108) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_04_021245) do
   create_table "breeds", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "gender"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
