a88021b865d756e6aab3e01dcbc20cd9e40723d2
Cortland Miller <cortlandm99@gmail.com>
2025-03-03 20:41:27 -0600

    Added validations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :adoptions
   has_many :dogs, through: :adoptions
+
+  validates :name, :address, :city, :province, presence: true
+  validates :phone_number, :email, presence: true, uniqueness: true
 end
