f584af2167bdcc38402f4ad4b4167f161d8cb1f1
Cortland Miller <cortlandm99@gmail.com>
2025-03-03 20:39:46 -0600

    Added validations to dog model.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,7 @@ class Dog < ApplicationRecord
 
   has_many :adoptions
   has_many :users, through: :adoptions
+
+  validates :name, :gender, presence: true
+  validates :age, numericality: { only_integer: true }
 end
