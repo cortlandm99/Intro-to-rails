faf10f6a9e7a71f1092e947b2e0028fbb4f348b9
Cortland Miller <cortlandm99@gmail.com>
2025-03-05 20:55:17 -0600

    Added ActiveAdmin resources for Breeds and Dogs.

---
+++ app/admin/dogs.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Dog do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :age, :gender, :description, :image_url, :breed_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :age, :gender, :description, :image_url, :breed_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
