bc5adb0eab199975f8e91cd7d4e97adab6690f64
Cortland Miller <cortlandm99@gmail.com>
2025-03-05 21:25:15 -0600

    Added a JSON fetch request to DogAPI for the breeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-require "open-uri"
+require "net/http"
+require "json"
 
 Adoption.destroy_all
 User.destroy_all
@@ -6,3 +7,8 @@ Dog.destroy_all
 Breed.destroy_all
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+breed_url = "https://dog.ceo/api/breeds/list/all"
+breed_uri = URI(breed_url)
+breed_response = Net::HTTP.get(breed_uri)
+JSON.parse(breed_response)['message'].keys
