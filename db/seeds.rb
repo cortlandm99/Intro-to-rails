075b5a577733bddcd25f8c3e74ded55af56b5aa5
Cortland Miller <cortlandm99@gmail.com>
2025-03-06 16:15:30 -0600

    Added logic to seeds.rb to generate dogs both when they have a main breed and a sub breed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Breed.destroy_all
 breed_url = "https://dog.ceo/api/breeds/list/all"
 breed_uri = URI(breed_url)
 breed_response = Net::HTTP.get(breed_uri)
-breeds = JSON.parse(breed_response)['message'].keys
+breeds = JSON.parse(breed_response)['message']
 
 breeds.each do |breed, sub_breeds|
   new_breed = Breed.find_or_create_by(name: breed)
@@ -35,6 +35,20 @@ breeds.each do |breed, sub_breeds|
     sub_breeds.each do |sub_breed|
       full_breed = "#{breed} #{sub_breed}"
       new_sub_breed = Breed.find_or_create_by(name: full_breed)
+
+      image_url = "https://dog.ceo/api/breed/#{sub_breed}/images/random"
+      image_uri = URI(image_url)
+      image_response = Net::HTTP.get(image_uri)
+      image = JSON.parse(image_response)
+
+      Dog.create(
+        name: Faker::Creature::Dog.name,
+        age: rand(1..25),
+        gender: Faker::Creature::Dog.gender,
+        breed: new_sub_breed,
+        description: Faker::Games::StreetFighter.quote,
+        image_url: image
+      )
     end
   end
 end
