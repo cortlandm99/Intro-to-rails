713ba7d94ba7a03bb490d8261ad094c79fc26061
Cortland Miller <cortlandm99@gmail.com>
2025-03-05 20:58:13 -0600

    Added ransackable attributes to dog, breed and adoption models.

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -3,4 +3,8 @@ class Adoption < ApplicationRecord
   belongs_to :user
 
   validates :adoption_date, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "adoption_date", "created_at", "dog_id", "id", "updated_at", "user_id" ]
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,4 +6,12 @@ class Dog < ApplicationRecord
 
   validates :name, :gender, presence: true
   validates :age, numericality: { only_integer: true }
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "age", "breed_id", "created_at", "description", "gender", "id", "id_value", "image_url", "name", "updated_at" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "adoptions", "breed", "users" ]
+  end
 end
