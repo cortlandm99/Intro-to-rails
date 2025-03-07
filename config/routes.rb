9c494571cdacf681a3e09a117c939e59cbd1be39
Cortland Miller <cortlandm99@gmail.com>
2025-03-07 13:04:14 -0600

    Added routing to a menu that leads to an about page and the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "about", to: "static_pages#about"
-  get "static_pages/home"
+  get "home", to: "static_pages#home"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
